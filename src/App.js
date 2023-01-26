5e3fdea7b3215666913094ae6962595205fd3d41
Zahra Hashimi <zahrakumailhashimi@gmail.com>
2023-01-26 12:20:57 -0500

    Implementing one digit operations

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CalculatorDisplay from './pages/CalculatorDisplay';
+
+
+function App() {
+  return (
+    <div className="container mx-auto mt-4 text-5xl">
+      <CalculatorDisplay />
+    </div>
+  );
+}
+
+export default App;
